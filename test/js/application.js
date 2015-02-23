c52ab732af5571c0066fddff49cec2b0a708854b
zhanj122 <jasmine.k.zhang@gmail.com>
2015-02-23 20:51:46 +0000

    sdf

---
+++ test/js/application.js
@@ -0,0 +1,21 @@
+// $() jquery is one entire function, the dollar sign represents the function
+
+say_hi('Vivian');
+
+// variables
+var name = 'Brian';
+var height = '6.1';
+// variable goes in parentheses ()
+// string goes in quotation marks ''
+// boolean is a true or false statement (i.e. x=0, 0==true -> true)
+        // 1 equal sign when you're setting, 2 when you're checking
+
+function say_hi(name) {
+    alert ('Hello ' + name);
+}
+
+// an array holds things
+// var dresser = ['socks', 'shirts', pants];
+// dresser[2] = pants
+// objects in an array start at 0
+
