a8457987516a508f7719181372da6d27c23481bb
zhanj122 <jasmine.k.zhang@gmail.com>
2015-02-27 14:36:04 +0000

    dsfsdf

--- 36_things/assets/js/entry1.js
+++ 36_things/assets/js/entry1.js
@@ -1,6 +1,1 @@
 document.getElementByClass('black-overlay').style.display='block';
-
-<a href="javascript:void(0)" onclick="document.getElementByClass('black-overlay').style.display='block';>Click it</a>
-
-<a href="javascript:void(0)" onclick="document.getElementByClass('black-overlay').style.display='none';>Hide it</a>
-
