4cb0447531a36df626a3034f12c0f9ff08d9a284
zhanj122 <jasmine.k.zhang@gmail.com>
2015-04-23 01:51:05 +0000

    generator

--- generator/generator.js
+++ generator/generator.js
@@ -1,30 +1,17 @@
-var cats = ["aa.jpg","baby.jpg","bike.jpg","blankie.jpg","chandler.jpg", "denver.JPG", "drake.jpg", "georgecostanza.png", "givenuppug.jpg", "givingup.jpg", "kdrama.jpg", "kevin.jpg", "patrick.png", "pugwholacksmotivationinlife.jpg", "sadpug.jpb", "screamingcat.jpg", "spoongeboob.jpg", "wetcat.jpg"];
+var faces = ["ross.jpg", "chicken.jpg", "pikachu.jpg", "death.jpg", "aa.jpg", "bike.jpg","blankie.jpg","chandler.jpg", "denver.JPG", "drake.jpg", "georgecostanza.png", "givenuppug.jpg", "givingup.jpg", "kdrama.jpg", "kevin.jpg", "patrick.png", "pugwholacksmotivationinlife.jpg", "sadpug.jpg", "screamingcat.jpg", "spoongeboob.jpg", "wetcat.jpg"];
  
-var poems = ["How do you feel?", "How are you today?", "What time did you sleep last night?", "How is school?", "Are you ready for finals?", "What do you want to do in life?"];
- 
-var selectedpoem = false;
+var questions = ["How do you feel?", "How are you today?", "When did you sleep last night?", "How is school?", "Are you ready for finals?", "What do you want to do in life?"];
+
+var selectedquestion = false;
  
 $(".go-button").click(function() {
-  var nextpoem = poems[Math.floor(Math.random()*poems.length)];
+  var nextquestion = questions[Math.floor(Math.random()*questions.length)];
   
-  while (selectedpoem == nextpoem) {
-    nextpoem = poems[Math.floor(Math.random()*poems.length)];
+  while (selectedquestion == nextquestion) {
+    nextquestion = questions[Math.floor(Math.random()*questions.length)];
   }
   
-  selectedpoem = nextpoem;
-  $(".poem-holder").html(nextpoem);
-  $(".cat-holder").html('<img src="' + cats[Math.floor(Math.random()*cats.length)]+ '">');
-  var newheight = (Math.floor(Math.random() * 100) + 100) + "px";
-  $(".line").css("height", newheight);
-  
-  for (var increment = 0; increment < 100; increment++) {
-    $("body").append("<div class='dot'></div>"); 
-  }
- 
-  $(".dot").each(function() {
-    var newtop = Math.floor(Math.random()*100) + "%";
-    var newleft = Math.floor(Math.random()*100) + "%";
-    $(this).css("top",newtop);
-    $(this).css("left",newleft);
-  });
-});
+  selectedquestion = nextquestion;
+  $(".question-holder").html(nextquestion);
+  $(".face-holder").html('<img src="' + faces[Math.floor(Math.random()*faces.length)]+ '">');
+}); 
