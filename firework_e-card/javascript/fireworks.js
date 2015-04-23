8ab33735853d18f8724c9b7fe03221140a0609e1
zhanj122 <jasmine.k.zhang@gmail.com>
2015-04-23 14:07:00 +0000

    add firework assets

--- firework_e-card/javascript/fireworks.js
+++ firework_e-card/javascript/fireworks.js
@@ -1,6 +1,35 @@
-$(document).click(function() {
-    var path  = 'assets/gif_firework_aqua.gif',
-        image = '<img src="' + path +'" />';
+//$(document).click(function() {
+//    var fireworks = ["assets/firework1.gif", "assets/firework2.gif"];
+//    var image = '<img src="' + fireworks +'" />';
+    
+//    img = '<img src="path/file.jpg">';
         
-   $('.background').append(image);
-});
+//   $('.background').append(image);
+//});
+
+
+var fireworks = ["assets/firework1.gif", "assets/firework2.gif", "assets/firework3.gif", "assets/firework4.gif", "assets/firework5.gif", "rocket.gif"];
+
+var selectedfirework = false;
+ 
+$(document).click(function() {
+  var nextfirework = fireworks[Math.floor(Math.random()*fireworks.length)];
+  
+  while (selectedfirework == nextfirework) {
+    nextfirework = fireworks[Math.floor(Math.random()*fireworks.length)];
+  }
+  
+  selectedfirework = nextfirework;
+  $(".background").html(nextfirework);
+  $(".background").html('<img src="' + fireworks[Math.floor(Math.random()*fireworks.length)]+ '">');
+}); 
+
+
+
+//$(document).click(function() {
+//    var path  = 'assets/firework1.gif',
+//        image = '<img src="' + path +'" />';
+        
+//   $('.background').append(image);
+//}); 
+
