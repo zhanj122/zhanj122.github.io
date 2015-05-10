097580dc1beb15ce5efe95b53c8f32b96c476fed
zhanj122 <jasmine.k.zhang@gmail.com>
2015-05-10 21:39:27 +0000

    all fireworks may 10th

--- firework_e-card/javascript/fireworks.js
+++ firework_e-card/javascript/fireworks.js
@@ -1,16 +1,40 @@
-var fireworks = ["assets/firework_disk_copper_2f_40fbreak_matte.gif", 
-                "assets/firework_disk_orange_2f_18fbreak_tweaked_matte.gif", 
-                "assets/firework_disk_teal_2f_24fbreak_matte.gif",
-                "assets/firework_flower_aqua_2f_60fbreak_matte.gif",
-                "assets/firework_flower_green_2f_18fbreak_tweaked_matte.gif",
-                "assets/firework_flower_magenta_4f_75fbreak_matte.gif",
-                "assets/firework_flower_neon_2f_40fbreak_tweaked_matte.gif",
-                "assets/firework_palm_blue_3f_23fbreak_matte.gif",
-                "assets/firework_palm_blue2_2f_25fbreak_matte.gif",
-                "assets/firework_palm_gold_3f_36fbreak_matte.gif",
-                "assets/shooting-star_2f_60f_matte.gif",
-                "assets/ufo.gif",
-                "assets/hot-air-balloon.gif"];
+var fireworks = ["assets/hot-air-balloon_loop1.gif",
+                "assets/firework_disk_copper_1f_80fbreak.gif",
+                "assets/firework_disk_copper_2f_40fbreak.gif",
+                "assets/firework_disk_copper_2f_loop1.gif",
+                "assets/firework_disk_copper_3f_45fbreak.gif",
+                "assets/firework_disk_orange_1f_28fbreak.gif",
+                "assets/firework_disk_orange_2f_18fbreak.gif",
+                "assets/firework_disk_orange_3f_28fbreak.gif",
+                "assets/firework_disk_orange_3f_loop1.gif",
+                "assets/firework_disk_teal_1f_24fbreak.gif",
+                "assets/firework_disk_teal_1f_loop1.gif",
+                "assets/firework_disk_teal_2f_24fbreak.gif",
+                "assets/firework_disk_teal_2f_loop1.gif",
+                "assets/firework_disk_teal_3f_24fbreak.gif",
+                "assets/firework_flower_aqua_1f_loop1.gif",
+                "assets/firework_flower_aqua_2f_60fbreak.gif",
+                "assets/firework_flower_aqua_2f_120fbreak.gif",
+                "assets/firework_flower_green_1f_60fbreak.gif",
+                "assets/firework_flower_green_2f_20fbreak.gif",
+                "assets/firework_flower_green_3f_36fbreak.gif",
+                "assets/firework_flower_green_4f_loop3.gif",
+                "assets/firework_flower_magenta_1f_45fbreak.gif",
+                "assets/firework_flower_magenta_2f_36fbreak.gif",
+                "assets/firework_flower_magenta_4f_75fbreak.gif",
+                "assets/firework_flower_magenta_5f_loop1.gif",
+                "assets/firework_flower_neon_1f_90fbreak.gif",
+                "assets/firework_flower_neon_1f_150fbreak.gif",
+                "assets/firework_flower_neon_2f_36fbreak.gif",
+                "assets/firework_palm_blue_3f_23fbreak.gif",
+                "assets/firework_palm_blue2_2f_25fbreak.gif",
+                "assets/firework_palm_gold_3f_36fbreak.gif",
+                "assets/hot-air-balloon_loop1.gif",
+                "assets/hot-air-balloon.gif",
+                "assets/shooting-star_2f_60f.gif",
+                "assets/shooting-star_loop1.gif",
+                "assets/ufo_loop1.gif",
+                "assets/ufo.gif"];
 
 var audio = ["assets/firework1.wav", 
             "assets/firework2.wav",
@@ -43,6 +67,7 @@ $('.record').click(function(event) {
     $(this).addClass('hide');
     $('.recording').removeClass('hide');
     $('.recording-message').addClass('show');
+    $('.time').removeClass('hide');
     $(".fireworks-container").html("");
     start_timer();
 });
